24feb3f36c5a51c517311d0e62b35d6036449b9a
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-11-27 01:16:39 -0300

    feat: :lipstick: update main page style and add music

--- 2023.1.devops-pre-hackathon/src/App.js
+++ 2023.1.devops-pre-hackathon/src/App.js
@@ -1,27 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isMainPageVisible, setIsMainPageVisible] = useState(false);
+
+  const handlePlay = () => {
+    setIsMainPageVisible(true);
+    const audio = new Audio(`${process.env.PUBLIC_URL}/music/hdst16.mp3`);
+    audio.play();
+    audio.loop = true;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DevOps Básico IT Talent - Atividade CI/CD 1
-        </p>
-        <p>
-          <b>Discente</b>: Gabriel Lins
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn DevOps
-        </a>
-      </header>
-    </div>
+    isMainPageVisible ? (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-Logo-Ctr">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <p>
+            DevOps Básico IT Talent - Atividade CI/CD 1
+          </p>
+          <p>
+            <b>Discente</b>: Gabriel Lins
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn DevOps
+          </a>
+        </header>
+      </div>
+    ) : (
+        <button className='animated-button6' onClick={handlePlay}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Clique aqui
+        </button>
+    )
   );
 }
 
